ecc8c54a73df277b37722ad6c8f27fa67ab38b45
zona, wenhsin, wu 吳文心 <w242920@gisbim.com>
2024-01-25 22:29:35 +0800

    first commit

---
+++ login.js
@@ -0,0 +1,29 @@
+import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js';
+
+createApp({
+    data(){
+        return {
+            user:{
+                username:'',
+                password:''
+            },
+        }
+    },
+    methods:{
+        login(){
+            console.log(1)
+            const api = 'https://vue3-course-api.hexschool.io/v2/admin/signin';
+            axios.post(api,this.user).then((response) => {
+                const { token,expired } = response.data;
+                document.cookie = `hexToken=${token};expires=${new Date(expired)}; path=/`;
+                window.location = 'products.html';
+            }).catch((err) =>{
+                alert(err.response.data.message);
+            });
+        },
+        test(text){
+            console.log(text)
+        }
+    },
+
+}).mount('#app');
